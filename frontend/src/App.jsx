25a136611d9fb97d60312a6ff0b2b814fd6d5732
Dulanjika <dulanjikabandara10@gmail.com>
2025-05-17 15:45:32 +0530

    Updated Navbar import path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import AdminDashboard from "./components/AdminDashboard";
+import InventoryManagement from "./pages/InventoryManagement";
+import InventoryRelease from "./pages/InventoryRelease";
+import PurchaseManagement from "./pages/PurchaseManagement";
+import InventoryStock from "./pages/InventoryStock";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,7 +16,11 @@ const App = () => {
   
   // Update the paths where Navbar should be hidden
  const hideNavbarPaths = [
-
+ "/admin-dashboard",
+ "/inventory-management",
+ "/inventory-release",
+ "/purchase-management",
+ "/inventory-stock",
   ];
 
   const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
@@ -21,7 +30,11 @@ const App = () => {
       {!shouldHideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
-       
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/inventory-management" element={<InventoryManagement />} />
+        <Route path="/inventory-release" element={<InventoryRelease />} />
+        <Route path="/purchase-management" element={<PurchaseManagement />} />
+        <Route path="/inventory-stock" element={<InventoryStock />} />
       </Routes>
       <ToastContainer
         position="top-right"
