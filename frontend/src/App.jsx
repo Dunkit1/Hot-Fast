f7c1f9b450bb6ac438137136b79e87cdd506d616
Dulanjika <dulanjikabandara10@gmail.com>
2025-05-17 16:06:51 +0530

    Updated App.js according to added pages' paths.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import InventoryManagement from "./pages/InventoryManagement";
 import InventoryRelease from "./pages/InventoryRelease";
 import PurchaseManagement from "./pages/PurchaseManagement";
 import InventoryStock from "./pages/InventoryStock";
+import ProductManagement from "./pages/ProductManagement";
+import ImageUploadPage from './pages/ImageUploadPage';
+import ProductInventoryRelease from "./pages/ProductInventoryRelease";
+import RecipeManagement from "./pages/RecipeManagement";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -21,6 +25,10 @@ const App = () => {
  "/inventory-release",
  "/purchase-management",
  "/inventory-stock",
+  "/product-management",
+  "/image-upload",
+  "/product-inventory-release",
+  "/recipe-management",
   ];
 
   const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
@@ -35,6 +43,11 @@ const App = () => {
         <Route path="/inventory-release" element={<InventoryRelease />} />
         <Route path="/purchase-management" element={<PurchaseManagement />} />
         <Route path="/inventory-stock" element={<InventoryStock />} />
+        <Route path="/product-management" element={<ProductManagement />} />
+        <Route path="/image-upload" element={<ImageUploadPage />} />
+        <Route path="/product-inventory-release" element={<ProductInventoryRelease />} />
+        <Route path="/recipe-management" element={<RecipeManagement />} />
+        
       </Routes>
       <ToastContainer
         position="top-right"
